118d4ee73d10931ff9da92cb884aa1daddaea73a
Texas R3X <texasr3xphone@gmail.com>
2024-12-08 16:28:47 -0700

    Code in FormModal.js cleaned up

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -5,7 +5,7 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
-import { useEffect, useMemo, useReducer, useRef, useState } from "react";
+import { useEffect, useMemo, useReducer, useRef } from "react";
 import createRandomID from "@/services/createRandomID";
 
 const ACTION_TYPES = {
@@ -16,19 +16,12 @@ const ACTION_TYPES = {
 const reducerFn = (inputValues, action) => {
     switch (action.type) {
         case ACTION_TYPES.RESET_VALUES: {
-            console.log("RESET_VALUES inputValues:", inputValues);
-
             return Array.from({ length: inputValues.length }).map(() => "");
         }
         case ACTION_TYPES.UPDATE_VALUE: {
-            console.log("UPDATE_VALUE action.newValue:", action.newValue);
-            console.log("UPDATE_VALUE action.index:", action.index);
-
             const newInputValues = [...inputValues]
 
             newInputValues[action.index] = action.newValue;
-
-            console.log("UPDATE_VALUE newInputValues:", newInputValues);
 
             return newInputValues;
         }
@@ -41,26 +34,17 @@ const reducerFn = (inputValues, action) => {
 export default function FormModal({ heading, submitLabel, isOpen, closeModalFn, textFieldData, verifyFn }) {
     const inputRefs = useRef([]);
     const [inputValues, dispatch] = useReducer(reducerFn, Array.from({ length: textFieldData.length }).map(() => ""));
-    // const [selectInput, setSelectInput] = useState(null);
 
-    console.log("inputRefs:", inputRefs);
-    console.log("0 inputValues:", inputValues);
 
     useEffect(() => {
         dispatch({ type: ACTION_TYPES.RESET_VALUES });
     }, [isOpen]);
 
-    const updateInputValue = (newValue, index) => {
-        console.log("newValue:", newValue);
-        console.log("index:", index);
-
-        // setSelectInput(index);
-        dispatch({
-            type: ACTION_TYPES.UPDATE_VALUE,
-            newValue,
-            index,
-        });
-    };
+    const updateInputValue = (newValue, index) => dispatch({
+        type: ACTION_TYPES.UPDATE_VALUE,
+        newValue,
+        index,
+    });
 
     const textFieldKeys = useMemo(() => {
         return Array.from({ length: inputValues.length }).map(() => createRandomID());
@@ -70,38 +54,29 @@ export default function FormModal({ heading, submitLabel, isOpen, closeModalFn, 
         if (event.key === "Enter") event.target.blur();
     }
 
-    const renderTextFields = () => {
-        return textFieldData.map((data, i) => {
+    // const renderTextFields = () => {
+    //     return textFieldData.map((data, i) => {
 
-            const elm = (
-                <TextField
-                    type="text"
-                    {...data}
-                    id={textFieldKeys[i]}
-                    key={textFieldKeys[i]}
-                    ref={(input) => inputRefs.current[i] = input}
-                    onBlur={(event) => updateInputValue(event.target.value, i)}
-                    onKeyDown={onEnterKey}
-                    defaultValue={inputValues[i]}
-                />
-            );
+    //         const elm = (
+    //             <TextField
+    //                 type="text"
+    //                 {...data}
+    //                 id={textFieldKeys[i]}
+    //                 key={textFieldKeys[i]}
+    //                 ref={(input) => inputRefs.current[i] = input}
+    //                 onBlur={(event) => updateInputValue(event.target.value, i)}
+    //                 onKeyDown={onEnterKey}
+    //                 defaultValue={inputValues[i]}
+    //             />
+    //         );
 
-            return elm;
-        });
-    }
-
-    // useEffect(() => {
-    //     console.log("e selectInput:", selectInput);
-    //     console.log("e inputRefs.current[selectInput]:", inputRefs.current[selectInput]);
-    //     console.log("True?:", selectInput !== null && !!inputRefs.current[selectInput]);
-    //     if (selectInput !== null && !!inputRefs.current[selectInput]) inputRefs.current[selectInput].focus();
-    // }, [selectInput]);
+    //         return elm;
+    //     });
+    // }
 
     const handleSubmit = () => {
         verifyFn(inputValues);
     }
-
-    console.log("1 inputValues:", inputValues);
 
     return isOpen ? (
         <Modal
@@ -125,10 +100,20 @@ export default function FormModal({ heading, submitLabel, isOpen, closeModalFn, 
                     className="text-field-container"
                     key={createRandomID()}
                 >
-                    {renderTextFields()}
+                    {textFieldData.map((data, i) => (
+                        <TextField
+                            type="text"
+                            {...data}
+                            id={textFieldKeys[i]}
+                            key={textFieldKeys[i]}
+                            ref={(input) => inputRefs.current[i] = input}
+                            onBlur={(event) => updateInputValue(event.target.value, i)}
+                            onKeyDown={onEnterKey}
+                            defaultValue={inputValues[i]}
+                        />
+                    ))}
                 </form>
 
-                inputValues: {inputValues.join(", ")}
                 <br/>
 
                 <Button
